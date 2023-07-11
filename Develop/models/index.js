de895d2cbc59b6786df484e5d993458835f7ed62
Sam DeGrandchamp <degrandchamps@gmail.com>
2023-07-10 20:33:34 -0400

    added product tags

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -13,9 +13,21 @@ Category.hasMany(Product, {
   foreignKey: 'category_id',
 });
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+  as: 'product_tags'
+});
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+  as: 'tag_products'
+});
 module.exports = {
   Product,
   Category,
